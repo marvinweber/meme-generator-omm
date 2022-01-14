6c6109dbe57e3d2a03c9f9e35f31c0ebfcde0e38
marvinweber <marvin@marvinweber.net>
2022-01-14 23:46:08 +0100

    Add tailwind support (with hot reloading)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='App w-full md:w-10/12 lg:w-8/12 m-auto' id='main'>
+      <div>
+        <h1 className='text-3xl'>Meme Generator</h1>
+      </div>
+      <div className='flex justify-between text-center h-12'>
+        <a className='bg-gray-400 flex-grow'>
+          <div>Meme Overview</div>
         </a>
-      </header>
+        <a className='bg-gray-600 flex-grow'>
+          <div>Meme Editor</div>
+        </a>
+        <a className='bg-gray-400 flex-grow'>
+          <div>Meme Overview</div>
+        </a>
+      </div>
     </div>
   );
 }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
