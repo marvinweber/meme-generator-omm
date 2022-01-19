a0e7729f79f2d74aaefe2d901d72642510ff2f65
marvinweber <marvin@marvinweber.net>
2022-01-19 16:46:31 +0100

    remove debug output

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -4,8 +4,6 @@ const router = express.Router();
 
 /* Endpoint to verify succesfull oauth login for clients. */
 router.get('/oauth/login', [requireAuthentication], (req, res, next) => {
-  console.log(req.authenticated);
-  console.log(req.user);
   res.json({
     success: true,
   });
