8c0a549f1f56c2cddbbc401ac247e3049a034740
marvinweber <marvin@marvinweber.net>
2022-02-04 17:38:21 +0100

    Add endpoint for adding template via image url

--- backend/src/controllers/templates.js
+++ backend/src/controllers/templates.js
@@ -1,3 +1,6 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import https from 'https';
 import path from 'path';
 import Template from '../models/template.js';
 import { ROOT_DIR } from '../app.js';
@@ -55,5 +58,75 @@ export const uploadTemplateWithFile = async (req, res, next) => {
 
 /** Template upload handler for urls. */
 export const createTemplateByImageUrl = async (req, res, next) => {
-  res.json('not yet implemented');
+  const imgUrl = req.body.templateUrl;
+  if (!imgUrl) {
+    return res.status(400).json({ success: false });
+  }
+
+  const now = new Date();
+  const fileExtention = path.extname(imgUrl);
+  const originalFilename = path.basename(imgUrl);
+
+  const downloadFilename =
+    crypto.createHash('md5').update(imgUrl).digest('hex') + fileExtention;
+  const downloadDir = path.join(
+    'uploads/templates',
+    `${now.getFullYear()}/${now.getMonth() + 1}`
+  );
+  const downloadPath = path.join(
+    ROOT_DIR,
+    'public',
+    downloadDir,
+    downloadFilename
+  );
+  try {
+    await downloadImage(imgUrl, downloadPath);
+  } catch (e) {
+    console.error(e);
+    return res
+      .status(400)
+      .json({ success: false, message: 'Could not download template image!' });
+  }
+
+  // create target file name (from md5 hash of image)
+  const imageBuffer = fs.readFileSync(downloadPath);
+  const targetFileName =
+    crypto.createHash('md5').update(imageBuffer).digest('hex') + fileExtention;
+  const targetPath = path.join(ROOT_DIR, 'public', downloadDir, targetFileName);
+  const uploadPath = path.join(downloadDir, targetFileName);
+
+  fs.renameSync(downloadPath, targetPath);
+
+  const template = await new Template({
+    uploadedAt: now,
+    originalFilename,
+    name: req.body.name || originalFilename,
+    path: uploadPath,
+    uploadUser: req.user._id,
+  })
+    .save()
+    .then((t) => t.populate('uploadUser', 'name'));
+
+  res.json({
+    success: true,
+    templates: [template],
+  });
+};
+
+const downloadImage = async (url, downloadPath) => {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      if (res.statusCode === 200) {
+        res
+          .pipe(fs.createWriteStream(downloadPath))
+          .on('error', reject)
+          .once('close', () => resolve(downloadPath));
+      } else {
+        res.resume();
+        reject(
+          new Error(`Request failed: ${res.statusCode} - ${res.statusMessage}`)
+        );
+      }
+    });
+  });
 };
