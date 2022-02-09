9b8c4926e1041331c079b53556f1406359f9e332
marvinweber <marvin@marvinweber.net>
2022-02-09 23:00:38 +0100

    adjust expandable design

--- frontend/src/components/util/Expandable.tsx
+++ frontend/src/components/util/Expandable.tsx
@@ -1,3 +1,5 @@
+import { mdiArrowDownCircleOutline, mdiArrowUpCircleOutline } from "@mdi/js";
+import Icon from "@mdi/react";
 import React, { useState } from "react";
 
 const Expandable: React.FC<{ heading: string; content: React.ReactNode }> = ({
@@ -7,20 +9,25 @@ const Expandable: React.FC<{ heading: string; content: React.ReactNode }> = ({
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="shadow-gray-500 shadow-md rounded-lg p-2">
-      <div className="flex justify-between">
+    <div className="rounded-lg p-2 border">
+      <div className="flex justify-between items-center">
         <h3 className="text-xl">{heading}</h3>
-        <button onClick={() => setOpen(!open)}>
-          {open ? "Close" : "Open"}
+        <button
+          onClick={() => setOpen(!open)}
+          className="p-1 rounded-md border border-slate-300 hover:bg-slate-200"
+        >
+          <div className="flex items-center">
+            <span className="text-slate-600 text-xs mr-1">
+              {open ? "Close" : "Open"}
+            </span>
+            <Icon
+              path={open ? mdiArrowUpCircleOutline : mdiArrowDownCircleOutline}
+              size={1}
+            />
+          </div>
         </button>
       </div>
-      {open ? (
-        <div className="border-t-2 p-2">
-          {content}
-        </div>
-      ) : (
-        <></>
-      )}
+      {open ? <div className="border-t-2 mt-2 p-2">{content}</div> : <></>}
     </div>
   );
 };
