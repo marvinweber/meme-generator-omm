bd106458276ed7b5d4c19a2d69c713435ea957df
marvinweber <marvin@marvinweber.net>
2022-02-10 16:49:31 +0100

    updated expandable design

--- frontend/src/components/util/Expandable.tsx
+++ frontend/src/components/util/Expandable.tsx
@@ -9,24 +9,18 @@ const Expandable: React.FC<{ heading: string; content: React.ReactNode }> = ({
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="rounded-lg p-2 border">
-      <div className="flex justify-between items-center">
+    <div className="rounded-lg border">
+      <div
+        onClick={() => setOpen(!open)}
+        className="flex p-2 justify-between items-center cursor-pointer
+                   hover:bg-slate-200 rounded-lg"
+      >
         <h3 className="text-xl">{heading}</h3>
-        <button
-          onClick={() => setOpen(!open)}
-          className="p-1 rounded-md border border-slate-300 hover:bg-slate-200"
-        >
-          <div className="flex items-center text-slate-600">
-            <span className="text-xs mr-1">
-              {open ? "Close" : "Open"}
-            </span>
-            <Icon
-              path={open ? mdiArrowUpCircleOutline : mdiArrowDownCircleOutline}
-              size={.9}
-              className="text-inherit"
-            />
-          </div>
-        </button>
+        <Icon
+          path={open ? mdiArrowUpCircleOutline : mdiArrowDownCircleOutline}
+          size={0.9}
+          className="text-inherit"
+        />
       </div>
       {open ? <div className="border-t mt-2 p-2">{content}</div> : <></>}
     </div>
