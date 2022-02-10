9946009d6f5cefe97dc4d13c766fa5102f662aab
marvinweber <marvin@marvinweber.net>
2022-02-10 21:18:54 +0100

    fix expandable test due to updated design/ux

--- frontend/src/components/util/Expandable.test.tsx
+++ frontend/src/components/util/Expandable.test.tsx
@@ -13,13 +13,13 @@ test("Expandable component renders without crashing", () => {
 
 test("Expanding feature of expandable component", async () => {
   const content = <p data-testid="test-123">Text Content 123</p>;
-  render(<Expandable heading="Test Expandable" content={content} />);
+  render(<Expandable heading="Heading" content={content} />);
 
   // check that content is hidden initially
   expect(screen.queryByTestId("test-123")).not.toBeInTheDocument();
 
   // click expand button and verify content element is now rendered
-  fireEvent.click(screen.getByRole("button"));
+  fireEvent.click(screen.getByText("Heading"));
   await screen.findByTestId("test-123");
   expect(screen.getByTestId("test-123")).toContainHTML('Text Content 123')
 });
